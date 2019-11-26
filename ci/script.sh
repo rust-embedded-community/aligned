6c9ec9351c59a053e2758b38fc884c1f67dd1cf3
Jorge Aparicio <jorge@japaric.io>
2019-11-26 17:13:48 -0500

    impl Index

--- ci/script.sh
+++ ci/script.sh
@@ -1,9 +1,9 @@
 set -euxo pipefail
 
 main() {
-    cargo check --target $TARGET
-
-    if [ $TRAVIS_RUST_VERSION = nightly ]; then
+    if [ $TARGET = x86_64-unknown-linux-gnu ]; then
+        cargo test --target $TARGET
+    else
         cargo check --target $TARGET
     fi
 }
